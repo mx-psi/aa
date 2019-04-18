6dc60f86a76b517c0dbc9a004322ca1ebeb51e43
Pablo <pbaeyens31@gmail.com>
2019-04-18 13:46:02 +0200

    Cambia patrón yapf

--- P2/trabajo2.py
+++ P2/trabajo2.py
@@ -6,9 +6,8 @@ Nombre Estudiante: Pablo Baeyens Fernández
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Fijamos la semilla
-np.random.seed(1)
+np.random.seed(2)
 
 
 def simula_unif(N, dim, rango):
@@ -35,7 +34,7 @@ def simula_recta(intervalo):
     y2 = points[1, 1]
     # y = a*x + b
     a = (y2 - y1) / (x2 - x1)  # Calculo de la pendiente.
-    b = y1 - a * x1       # Calculo del termino independiente.
+    b = y1 - a * x1  # Calculo del termino independiente.
 
     return a, b
 
@@ -103,14 +102,13 @@ scatter(x, title="Nube de puntos gaussiana")
 
 input("\n--- Pulsar tecla para continuar ---\n")
 
-
 ###############################################################################
 ###############################################################################
 ###############################################################################
-
 
 # EJERCICIO 1.2: Dibujar una gráfica con la nube de puntos de salida
 # correspondiente
+
 
 # La funcion np.sign(0) da 0, lo que nos puede dar problemas
 def signo(x):
@@ -129,17 +127,17 @@ N = 50
 a, b = simula_recta(intervalo)
 x = simula_unif(N, 2, intervalo)
 
-y = np.empty((N,))
+y = np.empty((N, ))
 for i in range(N):
     y[i] = f(x[i, 0], x[i, 1], a, b)
 
 vector_recta = np.array([b, a, -1])
 
-scatter(x, y,
+scatter(x,
+        y,
         ws=[vector_recta],
         labels_ws=["Frontera"],
         title="Puntos etiquetados en función de recta aleatoria")
-
 
 input("\n--- Pulsar tecla para continuar ---\n")
 
@@ -148,16 +146,16 @@ input("\n--- Pulsar tecla para continuar ---\n")
 y_noise = y.copy()
 
 # Modifica un 10% aleatorio de cada etiqueta
-for label in [-1,1]:
+for label in [-1, 1]:
     y_lab = np.nonzero(y == label)[0]
-    y_rand = np.random.choice(y_lab, len(y_lab)//10, replace = False)
+    y_rand = np.random.choice(y_lab, len(y_lab) // 10, replace=False)
     y_noise[y_rand] = -y_noise[y_rand]
 
-scatter(x, y_noise,
+scatter(x,
+        y_noise,
         ws=[vector_recta],
         labels_ws=["Frontera"],
         title="Puntos etiquetados con recta aleatoria (con ruido)")
-
 
 # CODIGO DEL ESTUDIANTE
 
@@ -172,26 +170,21 @@ input("\n--- Pulsar tecla para continuar ---\n")
 # recta
 
 
-def plot_datos_cuad(
-        X,
-        y,
-        fz,
-        title='Point cloud plot',
-        xaxis='x axis',
-        yaxis='y axis'):
-        # Preparar datos
+def plot_datos_cuad(X,
+                    y,
+                    fz,
+                    title='Point cloud plot',
+                    xaxis='x axis',
+                    yaxis='y axis'):
+    # Preparar datos
     min_xy = X.min(axis=0)
     max_xy = X.max(axis=0)
     border_xy = (max_xy - min_xy) * 0.01
 
     # Generar grid de predicciones
-    xx, yy = np.mgrid[min_xy[0] -
-                      border_xy[0]:max_xy[0] +
-                      border_xy[0] +
-                      0.001:border_xy[0], min_xy[1] -
-                      border_xy[1]:max_xy[1] +
-                      border_xy[1] +
-                      0.001:border_xy[1]]
+    xx, yy = np.mgrid[min_xy[0] - border_xy[0]:max_xy[0] + border_xy[0] +
+                      0.001:border_xy[0], min_xy[1] - border_xy[1]:max_xy[1] +
+                      border_xy[1] + 0.001:border_xy[1]]
     grid = np.c_[xx.ravel(), yy.ravel(), np.ones_like(xx).ravel()]
     pred_y = fz(grid)
     # pred_y[(pred_y>-1) & (pred_y<1)]
@@ -203,32 +196,33 @@ def plot_datos_cuad(
     ax_c = f.colorbar(contour)
     ax_c.set_label('$f(x, y)$')
     ax_c.set_ticks([-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1])
-    ax.scatter(X[:, 0], X[:, 1], c=y, s=50, linewidth=2,
-               cmap="RdYlBu", edgecolor='white')
+    ax.scatter(X[:, 0],
+               X[:, 1],
+               c=y,
+               s=50,
+               linewidth=2,
+               cmap="RdYlBu",
+               edgecolor='white')
 
     XX, YY = np.meshgrid(
-        np.linspace(
-            round(
-                min(min_xy)), round(
-                max(max_xy)), X.shape[0]), np.linspace(
-                    round(
-                        min(min_xy)), round(
-                            max(max_xy)), X.shape[0]))
+        np.linspace(round(min(min_xy)), round(max(max_xy)), X.shape[0]),
+        np.linspace(round(min(min_xy)), round(max(max_xy)), X.shape[0]))
     positions = np.vstack([XX.ravel(), YY.ravel()])
-    ax.contour(
-        XX, YY, fz(
-            positions.T).reshape(
-            X.shape[0], X.shape[0]), [0], colors='black')
+    ax.contour(XX,
+               YY,
+               fz(positions.T).reshape(X.shape[0], X.shape[0]), [0],
+               colors='black')
 
-    ax.set(
-        xlim=(min_xy[0] - border_xy[0], max_xy[0] + border_xy[0]),
-        ylim=(min_xy[1] - border_xy[1], max_xy[1] + border_xy[1]),
-        xlabel=xaxis, ylabel=yaxis)
+    ax.set(xlim=(min_xy[0] - border_xy[0], max_xy[0] + border_xy[0]),
+           ylim=(min_xy[1] - border_xy[1], max_xy[1] + border_xy[1]),
+           xlabel=xaxis,
+           ylabel=yaxis)
     plt.title(title)
     plt.show()
 
 
 # CODIGO DEL ESTUDIANTE
+
 
 def getProp(datos, labels, clasificador):
     """Obtiene la proporción de puntos correctamente clasificados
@@ -249,13 +243,14 @@ clasificadores = [
     (lambda x: (x[:, 0] - 10)**2 + (x[:, 1] - 20)**2 - 400, "Elipse 1"),
     (lambda x: 0.5 * (x[:, 0] - 10)**2 + (x[:, 1] - 20)**2 - 400, "Elipse 2"),
     (lambda x: 0.5 * (x[:, 0] - 10)**2 + (x[:, 1] + 20)**2 - 400, "Elipse 3"),
-    (lambda x: x[:, 1] - 20 * x[:, 0]**2 - 5 * x[:, 0] + 3, "Parábola")]
+    (lambda x: x[:, 1] - 20 * x[:, 0]**2 - 5 * x[:, 0] + 3, "Parábola")
+]
 
 # Representa y calcula el número bien clasificado para cada tipo
 for clasificador, title in clasificadores:
     plot_datos_cuad(x, y_noise, clasificador, title=title)
-    print("Proporción correcta para '{}': {}"
-          .format(title, getProp(x, y_noise, clasificador)))
+    print("Proporción correcta para '{}': {}".format(
+        title, getProp(x, y_noise, clasificador)))
 
 input("\n--- Pulsar tecla para continuar ---\n")
 
@@ -299,8 +294,8 @@ def ajusta_PLA(datos, label, max_iter, vini):
 
     return w
 
-# CODIGO DEL ESTUDIANTE
 
+# CODIGO DEL ESTUDIANTE
 
 # Random initializations
 iterations = []
@@ -308,17 +303,14 @@ for i in range(0, 10):
     pass
     # CODIGO DEL ESTUDIANTE
 
-print(
-    'Valor medio de iteraciones necesario para converger: {}'.format(
-        np.mean(
-            np.asarray(iterations))))
+print('Valor medio de iteraciones necesario para converger: {}'.format(
+    np.mean(np.asarray(iterations))))
 
 input("\n--- Pulsar tecla para continuar ---\n")
 
 # Ahora con los datos del ejercicio 1.2.b
 
 # CODIGO DEL ESTUDIANTE
-
 
 input("\n--- Pulsar tecla para continuar ---\n")
 
@@ -338,16 +330,12 @@ def sgdRL(TODO):
 # CODIGO DEL ESTUDIANTE
 input("\n--- Pulsar tecla para continuar ---\n")
 
-
 # Usar la muestra de datos etiquetada para encontrar nuestra solución g y estimar Eout
 # usando para ello un número suficientemente grande de nuevas muestras (>999).
 
-
 # CODIGO DEL ESTUDIANTE
 
-
 input("\n--- Pulsar tecla para continuar ---\n")
-
 
 ###############################################################################
 ###############################################################################
@@ -380,32 +368,42 @@ def readData(file_x, file_y, digits, labels):
 # Lectura de los datos de entrenamiento
 x, y = readData('datos/X_train.npy', 'datos/y_train.npy', [4, 8], [-1, 1])
 # Lectura de los datos para el test
-x_test, y_test = readData(
-    'datos/X_test.npy', 'datos/y_test.npy', [4, 8], [-1, 1])
-
+x_test, y_test = readData('datos/X_test.npy', 'datos/y_test.npy', [4, 8],
+                          [-1, 1])
 
 # mostramos los datos
 fig, ax = plt.subplots()
 ax.plot(np.squeeze(x[np.where(y == -1), 1]),
-        np.squeeze(x[np.where(y == -1), 2]), 'o', color='red', label='4')
-ax.plot(np.squeeze(x[np.where(y == 1), 1]), np.squeeze(
-    x[np.where(y == 1), 2]), 'o', color='blue', label='8')
-ax.set(xlabel='Intensidad promedio', ylabel='Simetria',
+        np.squeeze(x[np.where(y == -1), 2]),
+        'o',
+        color='red',
+        label='4')
+ax.plot(np.squeeze(x[np.where(y == 1), 1]),
+        np.squeeze(x[np.where(y == 1), 2]),
+        'o',
+        color='blue',
+        label='8')
+ax.set(xlabel='Intensidad promedio',
+       ylabel='Simetria',
        title='Digitos Manuscritos (TRAINING)')
 ax.set_xlim((0, 1))
 plt.legend()
 plt.show()
 
 fig, ax = plt.subplots()
-ax.plot(np.squeeze(x_test[np.where(y_test == -
-                                   1), 1]), np.squeeze(x_test[np.where(y_test == -
-                                                                       1), 2]), 'o', color='red', label='4')
-ax.plot(np.squeeze(x_test[np.where(y_test == 1), 1]), np.squeeze(
-    x_test[np.where(y_test == 1), 2]), 'o', color='blue', label='8')
-ax.set(
-    xlabel='Intensidad promedio',
-    ylabel='Simetria',
-    title='Digitos Manuscritos (TEST)')
+ax.plot(np.squeeze(x_test[np.where(y_test == -1), 1]),
+        np.squeeze(x_test[np.where(y_test == -1), 2]),
+        'o',
+        color='red',
+        label='4')
+ax.plot(np.squeeze(x_test[np.where(y_test == 1), 1]),
+        np.squeeze(x_test[np.where(y_test == 1), 2]),
+        'o',
+        color='blue',
+        label='8')
+ax.set(xlabel='Intensidad promedio',
+       ylabel='Simetria',
+       title='Digitos Manuscritos (TEST)')
 ax.set_xlim((0, 1))
 plt.legend()
 plt.show()
@@ -416,17 +414,13 @@ input("\n--- Pulsar tecla para continuar ---\n")
 
 # CODIGO DEL ESTUDIANTE
 
-
 input("\n--- Pulsar tecla para continuar ---\n")
-
 
 # POCKET ALGORITHM
 
 # CODIGO DEL ESTUDIANTE
 
-
 input("\n--- Pulsar tecla para continuar ---\n")
-
 
 # COTA SOBRE EL ERROR
 
